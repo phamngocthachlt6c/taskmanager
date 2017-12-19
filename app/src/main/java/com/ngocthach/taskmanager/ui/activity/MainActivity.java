895e29334e437980573e502e8e2b6d17dbb32d74
xuanson <xuanson@trycz.vn>
2017-12-19 10:51:41 +0700

    move swipe view to view package

--- app/src/main/java/com/ngocthach/taskmanager/ui/activity/MainActivity.java
+++ app/src/main/java/com/ngocthach/taskmanager/ui/activity/MainActivity.java
@@ -7,12 +7,11 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.ngocthach.taskmanager.R;
-import com.ngocthach.taskmanager.ui.SwipeViewPager;
+import com.ngocthach.taskmanager.ui.view.SwipeViewPager;
 import com.ngocthach.taskmanager.ui.adapter.SwipeViewAdapter;
 import com.ngocthach.taskmanager.ui.fragment.AddTaskFragment;
 import com.ngocthach.taskmanager.ui.fragment.CalendarFragment;
